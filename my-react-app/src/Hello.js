9633b6815434fffe67d4022051927948aa4ec311
Sijin063 <22000274@handong.ac.kr>
2025-11-10 20:28:24 +0900

    과제

---
+++ my-react-app/src/Hello.js
@@ -0,0 +1,7 @@
+import './Hello.css';
+
+function Hello() {
+  return <div className="hello">Hello, world!</div>;
+}
+
+export default Hello;
---
+++ my-react-app/src/New.js
@@ -0,0 +1,7 @@
+import './New.css';
+
+function New() {
+  return <div className="New">It's a new component!</div>;
+}
+
+export default New;
